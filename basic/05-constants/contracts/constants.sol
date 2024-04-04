4f2a5b5f4b6291688a1f42ea44c8dbfa9db02ae6
james-gates-0212 <james.gates.0212@gmail.com>
2024-04-04 04:47:59 -0700

    add: comments for constants📚

--- basic/05-constants/contracts/constants.sol
+++ basic/05-constants/contracts/constants.sol
@@ -1,6 +1,8 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.24;
 
+// Constants are variables that cannot be modified.
+// Their value is hard coded and using constants can save gas cost.
 contract Constants {
     // coding convention to uppercase constant variables
     address public constant MY_ADDRESS = 0x777788889999AaAAbBbbCcccddDdeeeEfFFfCcCc;
