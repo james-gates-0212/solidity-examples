40d6772c95c0715d6d69ffd96a311e1f1834a68a
james-gates-0212 <james.gates.0212@gmail.com>
2024-04-18 05:47:46 -0700

    :triangular_flag_on_post: add: contract Assembly Conditional Statements
    Co-Authored-By: clive-goldminer <161703670+clive-goldminer@users.noreply.github.com>
    Co-Authored-By: dragon-8800 <159820590+dragon-8800@users.noreply.github.com>

---
+++ basic/48-assembly-conditional-statements/contracts/AssemblyIf.sol
@@ -0,0 +1,31 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.24;
+
+contract AssemblyIf {
+    function yul_if(uint256 x) public pure returns (uint256 z) {
+        assembly {
+            // if condition = 1 { code }
+            // no else
+            // if 0 { z := 99 }
+            // if 1 { z := 99 }
+            if lt(x, 10) {
+                z := 99
+            }
+        }
+    }
+
+    function yul_switch(uint256 x) public pure returns (uint256 z) {
+        assembly {
+            switch x
+            case 1 {
+                z := 10
+            }
+            case 2 {
+                z := 20
+            }
+            default {
+                z := 0
+            }
+        }
+    }
+}
